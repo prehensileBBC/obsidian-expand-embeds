106421bee6f90812836e86c256139d284841ff33
prehensileBBC <henry.cooke@bbc.co.uk>
2023-07-07 13:29:12 +0100

    first working version!

--- main.ts
+++ main.ts
@@ -1,48 +1,53 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, getLinkpath, PluginManifest, Vault, MetadataCache } from 'obsidian';
+import slugify from "@sindresorhus/slugify";
+import Path from 'path';
+import { link } from 'fs';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface ExpandEmbedPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ExpandEmbedPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ExpandEmbedPlugin extends Plugin {
+
+	settings: ExpandEmbedPluginSettings;
+
+	frontmatterRegex = /^\s*?---\n([\s\S]*?)\n---/g;
+	transcludedRegex = /!\[\[(.+?)\]\]/g;
+
+	/*
+	constructor( app:App, manifest:PluginManifest, settings: ExpandEmbedPluginSettings) {
+        super( app, manifest );
+		this.vault = app.vault;
+        this.metadataCache = metadataCache;
+        this.settings = settings;
+    }*/
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'expand-embeds-selection',
+			name: 'Expand embedded notes in current selection',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+
+				const linkName = Path.parse( this.app.workspace.getActiveFile().name ).name;
+				const linkPath = getLinkpath( linkName );
+
+				this.expandEmbedsInSelection(
+					editor.getSelection(),
+					linkPath,
+					0, 4 )
+					.then( (expandedText: string) => {
+						editor.replaceSelection( expandedText );
+					}
+				);
+	
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
@@ -56,7 +61,8 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						//new SampleModal(this.app).open();
+						console.log( "Do the operation!" );
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -65,17 +71,6 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,49 +84,101 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	async expandEmbedsInSelection( selection: string, filePath: string, currentDepth: number, maxDepth:number ): Promise<string> {
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		// based on code lifted from https://github.com/oleeskild/obsidian-digital-garden
+		
+		if ( currentDepth >= maxDepth ) {
+			console.warn( `expandEmbedsInSelection hit maxDepth: ${maxDepth}` );
+            return selection;
+        }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		const transclusionMatches = selection.match(this.transcludedRegex);
+		let expandedText = selection.slice(); // copy the original text
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+		if (transclusionMatches) {
+			for (let i = 0; i < transclusionMatches.length; i++) {
+				try {
+	
+					const transclusionMatch = transclusionMatches[i];
+					const [tranclusionFileName, headerName] = transclusionMatch.substring(transclusionMatch.indexOf('[') + 2, transclusionMatch.indexOf(']')).split("|");
+					const tranclusionFilePath = getLinkpath(tranclusionFileName);
+					const linkedFile = this.app.metadataCache.getFirstLinkpathDest(tranclusionFilePath, filePath);
+					
+					let sectionID = "";
+					if (linkedFile.extension === "md") {
+	
+						let fileText = await this.app.vault.cachedRead(linkedFile);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+						if (tranclusionFileName.includes('#^')) {
+							// Transclude Block
+							const metadata = this.app.metadataCache.getFileCache(linkedFile);
+							const refBlock = tranclusionFileName.split('#^')[1];
+							sectionID = `#${slugify(refBlock)}`;
+							const blockInFile = metadata.blocks[refBlock];
+							if (blockInFile) {
+	
+								fileText = fileText
+									.split('\n')
+									.slice(blockInFile.position.start.line, blockInFile.position.end.line + 1)
+									.join('\n').replace(`^${refBlock}`, '');
+							}
+						
+						} else if (tranclusionFileName.includes('#')) { // transcluding header only
+							const metadata = this.app.metadataCache.getFileCache(linkedFile);
+							const refHeader = tranclusionFileName.split('#')[1];
+							const headerInFile = metadata.headings?.find(header => header.heading === refHeader);
+							sectionID = `#${slugify(refHeader)}`;
+							if (headerInFile) {
+								const headerPosition = metadata.headings.indexOf(headerInFile);
+								// Embed should copy the content proparly under the given block
+								const cutTo = metadata.headings.slice(headerPosition + 1).find(header => header.level <= headerInFile.level);
+								if (cutTo) {
+									const cutToLine = cutTo?.position?.start?.line;
+									fileText = fileText
+										.split('\n')
+										.slice(headerInFile.position.start.line, cutToLine)
+										.join('\n');
+								} else {
+									fileText = fileText
+										.split('\n')
+										.slice(headerInFile.position.start.line)
+										.join('\n');
+								}
+	
+							}
+						}
+						//Remove frontmatter from transclusion
+						fileText = fileText.replace(this.frontmatterRegex, "");
+	
+						// Apply custom filters to transclusion
+						// fileText = await this.convertCustomFilters(fileText);
+	
+						// const header = this.generateTransclusionHeader(headerName, linkedFile);
+	
+						// const headerSection = header ? `$<div class="markdown-embed-title">\n\n${header}\n\n</div>\n` : '';
+						// let embedded_link = "";
+						// if (publishedFiles.find((f) => f.path == linkedFile.path)) {
+						// 	embedded_link = `<a class="markdown-embed-link" href="/${generateUrlPath(getGardenPathForNote(linkedFile.path, this.rewriteRules))}${sectionID}" aria-label="Open link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></a>`;
+						// }
+						// fileText = `\n<div class="transclusion internal-embed is-loaded">${embedded_link}<div class="markdown-embed">\n\n${headerSection}\n\n`
+						// 	+ fileText + '\n\n</div></div>\n'
+						
+						//This should be recursive up to a certain depth
+						if (fileText.match(this.transcludedRegex)) {
+							fileText = await this.expandEmbedsInSelection( fileText, linkedFile.path, currentDepth + 1, maxDepth );
+						}
+						
+						expandedText = expandedText.replace(transclusionMatch, fileText);
+					}
+				} catch (error) {
+					console.error(error);
+					continue;
+				}
+			}
+		}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return expandedText;
 	}
 }
